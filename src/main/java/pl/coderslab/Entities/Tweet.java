9eb9cb703cc1f511f6df0a28ce71c48ee1cc025b
bruno <bruno.kedzierski@wp.pl>
2018-10-01 19:26:00 +0200

    Start

---
+++ src/main/java/pl/coderslab/Entities/Tweet.java
@@ -0,0 +1,10 @@
+package pl.coderslab.Entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+//
+//@Entity
+//@Table(name = "tweets")
+//public class Tweet {
+//
+//}
---
+++ src/main/java/pl/coderslab/Entities/User.java
@@ -0,0 +1,16 @@
+package pl.coderslab.Entities;
+
+import javax.persistence.*;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String username;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false, unique = true)
+    private String email;
+}
